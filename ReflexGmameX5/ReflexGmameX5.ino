4ee37451f2e54c561d2034f66cb9993b49295969
Michał Kozłowski <kozolex@gmail.com>
2019-03-30 11:25:54 +0100

    v1.1
    -Change table of results
    -Show "NEW RECORD"
    -Corect the Code
    -Translate PL->EN

--- ReflexGmameX5/ReflexGmameX5.ino
+++ ReflexGmameX5/ReflexGmameX5.ino
@@ -1,41 +1,39 @@
-#include <LiquidCrystal_I2C.h>
-
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h> //https://github.com/junwha0511/LiquidCrystal_I2C_Hangul
+#include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 
-int Lampka = 0, LampkaPoprzednia = 20;
-float koniecCzas;
-int sredniCzas;
-int szybkiCzas = 10000, wolnyCzas;
-int liczbaRund = 50;
-unsigned long  koniecCzasGry, poczatekCzasGry, poczatekCzasPunktu, koniecCzasPunktu, rekordCzas =5000 ;
+int led = 0, ledBefore = 20;
+float timeGameAll, timeRecord = 99000;
+int timeAverage;
 
-
-bool decyzja = false;
+int countRound = 5;
+unsigned long  timeGameStart, timeGameEnd, timePointStart, timePointEnd ;
+bool pointDecision = false;
 short int i, point = 0;
-short int Pozwolenie = 0;
+short int pointAllow = 0;
 
-void point_count() { // Funkcja dodaj�ca punkty
-
-  decyzja = true;
+void point_count() {        // Add points
+  pointDecision = true;
 }
-void powtarzanie() {  // Funkcja zapobiegaj�ca powtarzaniu si� tych samych diod
-  while (LampkaPoprzednia == Lampka) {
-    if (Lampka == 4) {
-      Lampka = 0;
+
+void avoid_Random() {        // Avoid repeated this same led in sequence
+  while (ledBefore == led) {
+    if (led == 4) {
+      led = 0;
     }
     else {
-      Lampka++;
+      led++;
     }
   }
 }
 
-void setup() {    //   Inicjacja.
-  lcd.init();
+void setup() {
+  lcd.begin();
   lcd.backlight();
   lcd.setCursor(0, 0);
-  lcd.print( "- - WITAJ GRACZU - -");
+  lcd.print( "  - - WELCOME - - ");
+  lcd.setCursor(0, 1);
+  lcd.print( "  REFLEX GAME 1.1   ");
   randomSeed(analogRead(A0));
   Serial.begin(9600);
   pinMode(3, OUTPUT);
@@ -47,143 +45,138 @@ void setup() {    //   Inicjacja.
   attachInterrupt(digitalPinToInterrupt(2), point_count, RISING);
 }
 
-void start_routine() { // Funkcja startuj�ca program. Wykonuj� si� tu �wiecenia 3 razy wszystkie diody.
-  int szybkosc = 100;
+void start_routine() {            // Animation effect at start
+  int timeDelay = 100;
   for (i = 3; i > 0; i--)
   {
     for (int j = 3; j < 8; j++) {
       digitalWrite(j, HIGH);
-      delay(szybkosc);
+      delay(timeDelay);
     }
     for (int j = 3; j < 8; j++) {
       digitalWrite(j, LOW);
-      delay(szybkosc);
+      delay(timeDelay);
     }
     lcd.setCursor(0, 1);
     lcd.print("- - - - - - - - - -");
     lcd.setCursor(9, 1);
     lcd.print(i);
-    delay(500);
+    delay(4);
     for (int j = 3; j < 8; j++) {
       digitalWrite(j, LOW);
     }
-    delay(500);
+    delay(300);
   }
   for (int j = 3; j < 8; j++) {
     digitalWrite(j, HIGH);
   }
-  delay(500);
+  delay(300);
   for (int j = 3; j < 8; j++) {
     digitalWrite(j, LOW);
   }
-  delay(500);
-  Serial.println(" - - - - - GO - - - - - - ");
+  delay(300);
 }
 
-void loop() {   // Funkcja g��wna
-  decyzja = false;
-  String linia1;
-  lcd.init();
+void loop() {  
+  pointDecision = false;
+  int timeFastReaction = 10000, timeSlowReaction = 0;
+  String lineString;
+  lcd.begin();
   point = 0;
-  start_routine();       //Funkcja start
+  start_routine();
   lcd.setCursor(0, 1);
   lcd.print( "                    ");
-  lcd.setCursor(0, 0);
-  lcd.print( "- - - - GO - - - - -");
-  poczatekCzasGry = millis();           //***************Poczatek gry
-  for (i = 0; i < liczbaRund; i++) {     //P�tla w kt�rej nast�puje losowanie przycisk�w
-    Lampka = random(5);
-    powtarzanie();
-    Pozwolenie = point;
-    poczatekCzasPunktu = millis();     //***************poczatek czasu zliczania refleksu
-    digitalWrite(Lampka + 3, HIGH);
+  timeGameStart = millis();        
+  for (i = 0; i < countRound; i++) {     //Random loop
+    led = random(5);
+    avoid_Random();
+    pointAllow = point;
+    timePointStart = millis();           //Start mesure reaction time
+    digitalWrite(led + 3, HIGH);
     lcd.setCursor(0, 2);
-    if (liczbaRund - point <10) 
-    linia1 = "     ->  " + String(liczbaRund - point) + " <-       ";
-    else 
-    linia1 = "     -> " + String(liczbaRund - point) + " <-       ";
-    
-    lcd.print(linia1);
-    while (decyzja == false) {   // P�tla kt�ra stopuje program. Uczestnik musi wcisn�� przycisk, aby kolejna z dioda zapali�� si�.
-      delay(0);
+    if (countRound - point < 10)
+      lineString = "      ->  " + String(countRound - point) + " <-       ";
+    else
+      lineString = "      -> " + String(countRound - point) + " <-       ";
+
+    lcd.print(lineString);
+      lcd.setCursor(0,0);
+      lcd.print("         Best:" + String (timeRecord/1000) + "s");
+    while (pointDecision == false) {   // Wait until correct button will be push
+      lcd.setCursor(0,0);
+      lcd.print("Time:" + String ((millis()-timeGameStart)/1000) + "s");
     }
-    digitalWrite(Lampka + 3, LOW);
-    decyzja = false;
-    koniecCzasPunktu = millis();
+    digitalWrite(led + 3, LOW);
+    pointDecision = false;
+    timePointEnd = millis();
     point = point + 1;
     delay(50);
-    // Warunki sprawdzaj�ce czas reakcji. Sprawdzaj� one czas najszybszy i najwolniejszy.
-    if ((koniecCzasPunktu - poczatekCzasPunktu) > wolnyCzas) { //Najwolniejszy czas
-      wolnyCzas = koniecCzasPunktu - poczatekCzasPunktu;
+    // Conditions to reaction mesure
+    if ((timePointEnd - timePointStart) > timeSlowReaction) { //Slowest reaction
+      timeSlowReaction = timePointEnd - timePointStart;
     }
-    if ((koniecCzasPunktu - poczatekCzasPunktu) < szybkiCzas) { // Najszybszy czas
-      szybkiCzas = koniecCzasPunktu - poczatekCzasPunktu;
+    if ((timePointEnd - timePointStart) < timeFastReaction) { // Fastest reaction
+      timeFastReaction = timePointEnd - timePointStart;
     }
-    LampkaPoprzednia = Lampka;
+    ledBefore = led;      //save to memory last led to avoid repeated the same led at sequence
   }
-  koniecCzasGry = millis();
-  koniecCzas = koniecCzasGry - poczatekCzasGry;
-  sredniCzas = koniecCzas / liczbaRund;
-
-  // Poni�ej wypisanie do portu szeregowego wynik�w.
-  Serial.print(" Wynik = ");  Serial.println(point);  Serial.print( " Czas = "); Serial.print(koniecCzas / 1000) ; Serial.println( "sekund.");
-  Serial.print(" Sredni czas reakcji = ");  Serial.println(sredniCzas);
-  Serial.print(" Najszybsza reakcja = ");  Serial.println(szybkiCzas);
-  Serial.print(" Najwolniejsza reakcja = ");  Serial.println(wolnyCzas);
+  timeGameEnd = millis();
+  timeGameAll = timeGameEnd - timeGameStart;
+  timeAverage = timeGameAll / countRound;
 
   int k ;
-  // Poni�ej wy�wietlenie wynik�w na LCD.
-  int zwloka = 1000;
-  
-  lcd.init();
-  //lcd.setCursor(0, 0);
-  //lcd.print("Wynik:");
-  //lcd.setCursor(6, 0);
-  //lcd.print(point);
-  delay(zwloka);
+  // Show the results on LCD
+  int timeDelayShowResults = 1000;
+  lcd.begin();
+  delay(timeDelayShowResults);
   lcd.setCursor(0, 0);
-  linia1 = "  Czas: " + String(koniecCzas / 1000) + " s";
-  lcd.print(linia1);
-  //lcd.setCursor(15, 0);
-  //lcd.print(koniecCzas / 1000);
-  delay(zwloka);
+  lineString = "You:" + String(timeGameAll / 1000) + " Best:" + String(timeRecord / 1000);
+  lcd.print(lineString);
+  delay(timeDelayShowResults);
   lcd.setCursor(0, 1);
-  if(sredniCzas <1000){
-    linia1 = "  Reflex : " + String(sredniCzas) + " ms";
+  if (timeAverage < 1000) {
+    lineString = "HI : " + String(timeFastReaction) + " ms";
   }
-  else{
-    linia1 = "  Reflex: " + String(sredniCzas) + " ms";
+  else {
+    lineString = "HI: " + String(timeFastReaction) + " ms";
   }
-  lcd.print(linia1);
-  //lcd.setCursor(8, 1);
-  //lcd.print(sredniCzas);
-  delay(zwloka);
-  if (rekordCzas > sredniCzas) rekordCzas=sredniCzas;
-    lcd.setCursor(0, 2);
-  if(sredniCzas <1000){
-    linia1 = "  Rekord : " + String(rekordCzas) + " ms";
+  lcd.print(lineString);
+  delay(timeDelayShowResults);
+  float timeRecordOld = timeRecord;
+  if (timeRecord > timeGameAll)
+  {
+    timeRecord = timeGameAll;
   }
-  else{
-    linia1 = "  Rekord: " + String(rekordCzas) + " ms";
+  lcd.setCursor(0, 2);
+  if (timeAverage < 1000) {
+    lineString = "LOW: " + String(timeSlowReaction) + " ms";
   }
-  lcd.print(linia1);
-  /*lcd.setCursor(0, 2);
-    lcd.print("Refleks max:   ");
-    lcd.setCursor(12, 2);
-    lcd.print(szybkiCzas);
-    lcd.setCursor(15,2);
-    lcd.print("ms");
-    delay(zwloka);
-  */
+  else {
+    lineString = "LOW: " + String(timeSlowReaction) + " ms";
+  }
+  lcd.print(lineString);
 
-  delay(zwloka);
-  while (decyzja == false) {   // P�tla kt�ra stopuje program. Uczestnik musi wcisn�� przycisk, aby kolejna z dioda zapali�� si�.
+  delay(timeDelayShowResults);
+  while (pointDecision == false) {   // Wainting to next game (please press button
     digitalWrite(5, HIGH);
     lcd.setCursor(0, 3);
-    lcd.print("NACISNIJ    PRZYCISK");
+    lcd.print("PRESS   THE   BUTTON");
+    if (timeRecord < timeRecordOld){
+          lcd.setCursor(17, 1);
+          lcd.print("NEW");
+          lcd.setCursor(14, 2);
+          lcd.print("RECORD");
+    }
+    
     delay(100);
     lcd.setCursor(0, 3);
     lcd.print("                    ");
+        if (timeRecord < timeRecordOld){
+          lcd.setCursor(17, 1);
+          lcd.print("   ");
+          lcd.setCursor(14, 2);
+          lcd.print("      ");
+    }
     digitalWrite(5, LOW);
     delay(100);
   }
